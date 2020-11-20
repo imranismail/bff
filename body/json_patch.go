42598ece601db9be1effe575e46177f6147d116d
Imran Ismail <imran.codely@gmail.com>
2020-11-20 19:50:29 +0800

    chore: pin jsonpatch to v5

--- body/json_patch.go
+++ body/json_patch.go
@@ -6,7 +6,7 @@ import (
 	"io/ioutil"
 	"net/http"
 
-	jsonpatch "github.com/evanphx/json-patch"
+	jsonpatch "github.com/evanphx/json-patch/v5"
 	"github.com/google/martian/v3/log"
 	"github.com/google/martian/v3/parse"
 )
