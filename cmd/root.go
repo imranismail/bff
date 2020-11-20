eb31dcaf681e98b3a0e11d1292a031988d6081d0
Imran Ismail <imran.codely@gmail.com>
2020-11-20 19:44:23 +0800

    chore: update cli description

--- cmd/root.go
+++ cmd/root.go
@@ -20,13 +20,10 @@ var cfgPath = path.Join(xdg.ConfigHome, "bff")
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "bff",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Backend for frontend",
+	Long: `
+An API-aware proxy that is cabaple of routing, filtering, verifying
+and modifing HTTP request and response.`,
 	Run: proxy.Serve,
 }
 
